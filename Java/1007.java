7750a1369f6d752b5a466b790e8e7a57c7c57fd4
Raihan Shah <47655426+Q027@users.noreply.github.com>
2024-08-12 00:22:10 +0600

    Create 1007.java

---
+++ Java/1007.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       int A = sc.nextInt();
+       int B = sc.nextInt();
+       int C = sc.nextInt();
+       int D = sc.nextInt();
+       int X=(A * B - C * D);
+       System.out.println("DIFERENCA = "+X);
+    }
+}
