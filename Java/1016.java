f28503397625e377e8361078a56c129fffd83821
Raihan Shah <47655426+Q027@users.noreply.github.com>
2024-08-12 13:13:47 +0600

    Create 1016.java

---
+++ Java/1016.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+int a=sc.nextInt();
+int minute=a*2;
+System.out.println(minute+" minutos");
+
+    }
+}
