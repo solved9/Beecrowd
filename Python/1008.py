dbfb73c8d10878ea32fc5c4c237e3f1f65ec0899
No Name <47655426+solved9@users.noreply.github.com>
2024-10-03 11:57:17 +0600

    Delete Python directory

--- Python/1008.py
+++
@@ -1,6 +0,0 @@
-a=int(input())
-b=int(input())
-c=float(input())
-d=b*c
-print("NUMBER =",a)
-print("SALARY = U$ %.2f" %d)
--- Python/1009.py
+++
@@ -1,6 +0,0 @@
-a=str(input())
-b=float(input())
-c=float(input())
-d=(c*15)/100
-e=b+d
-print("TOTAL = R$ %.2f" %e)
